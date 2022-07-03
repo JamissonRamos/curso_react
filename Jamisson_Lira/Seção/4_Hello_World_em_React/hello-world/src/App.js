4ccd53f2696f61016140d893c5ef1e7c3582abc0
Jamisson Lira Ramos <missonlira@gmail.com>
2022-07-03 19:14:36 -0300

    atualizando dados

--- Jamisson_Lira/Seção/4_Hello_World_em_React/hello-world/src/App.js
+++ Jamisson_Lira/Seção/4_Hello_World_em_React/hello-world/src/App.js
@@ -6,13 +6,16 @@ function App() {
     <div className="App">
 
       <h1> Hello world </h1>
+
       <h2> Jamisso Lira Ramos </h2>
 
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti saepe ut dignissimos non, placeat dolore
-          fuga nobis possimus corporis. Labore maiores itaque beatae quaerat odit saepe
-          dolorum officiis nesciunt reiciendis?</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti saepe ut dignissimos non, placeat dolore
+        fuga nobis possimus corporis. Labore maiores itaque beatae quaerat odit saepe
+        dolorum officiis nesciunt reiciendis?
+      </p>
 
-          <h1> teste </h1>
+        <h1> Fim do teste  </h1>
           
     </div>
   );
