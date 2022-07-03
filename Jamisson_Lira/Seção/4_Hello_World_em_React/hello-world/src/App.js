7c972f920a3024a8471addfb1c485b49f5fe3ae7
Jamisson Lira Ramos <missonlira@gmail.com>
2022-07-03 17:23:37 -0300

    configuraçãodo vs code

--- Jamisson_Lira/Seção/4_Hello_World_em_React/hello-world/src/App.js
+++ Jamisson_Lira/Seção/4_Hello_World_em_React/hello-world/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello world
-      </h1>
-      <h2>
-        Jamisso Lira Ramos
-      </h2>
+
+      <h1> Hello world </h1>
+      <h2> Jamisso Lira Ramos </h2>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti saepe ut dignissimos non, placeat dolore
+          fuga nobis possimus corporis. Labore maiores itaque beatae quaerat odit saepe
+          dolorum officiis nesciunt reiciendis?</p>
+          
     </div>
   );
 }
