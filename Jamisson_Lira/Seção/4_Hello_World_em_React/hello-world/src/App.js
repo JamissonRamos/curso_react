bfbb2253dd0c8b8b626bfd205b970cac63b62d6d
Jamisson Lira Ramos <missonlira@gmail.com>
2022-07-03 14:15:23 -0300

    Criando Hello World

---
+++ Jamisson_Lira/Seção/4_Hello_World_em_React/hello-world/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello world
+      </h1>
+      <h2>
+        Jamisso Lira Ramos
+      </h2>
+    </div>
+  );
+}
+
+export default App;
